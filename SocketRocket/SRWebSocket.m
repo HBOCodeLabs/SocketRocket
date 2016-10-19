d5375bd215faa101930062ad4bdd3947a77ac7a7
Jamie Laflen <jamie.laflen@hbo.com>
2016-10-19 16:17:22 -0700

    Migrate to xcode8

--- SocketRocket/SRWebSocket.m
+++ SocketRocket/SRWebSocket.m
@@ -526,7 +526,7 @@ static __strong NSData *CRLFCRLF;
     CFHTTPMessageSetHeaderFieldValue(request, CFSTR("Host"), (__bridge CFStringRef)(_url.port ? [NSString stringWithFormat:@"%@:%@", _url.host, _url.port] : _url.host));
         
     NSMutableData *keyBytes = [[NSMutableData alloc] initWithLength:16];
-    SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes);
+    assert(SecRandomCopyBytes(kSecRandomDefault, keyBytes.length, keyBytes.mutableBytes) == 0);
     _secKey = [keyBytes SR_stringByBase64Encoding];
     assert([_secKey length] == 24);
     
@@ -1359,7 +1359,7 @@ static const size_t SRFrameHeaderOverhead = 32;
         }
     } else {
         uint8_t *mask_key = frame_buffer + frame_buffer_size;
-        SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key);
+        assert(SecRandomCopyBytes(kSecRandomDefault, sizeof(uint32_t), (uint8_t *)mask_key) == 0);
         frame_buffer_size += sizeof(uint32_t);
         
         // TODO: could probably optimize this with SIMD
